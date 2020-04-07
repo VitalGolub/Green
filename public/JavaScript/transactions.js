8d667b0542f62c22dae8ee707a0023238f33cd5e
Atharva Shinde <atharva93@hotmail.com>
2020-04-07 19:46:44 -0400

    fixed some css

--- public/JavaScript/transactions.js
+++ public/JavaScript/transactions.js
@@ -1,7 +1,6 @@
 
 window.onload = function() {
     document.getElementById("newTransaction").onclick = function() {
-        // let table = document.getElementByTagName("table");
         let date = document.getElementById('date').value;
         let description = document.getElementById('description').value;
         let category = document.getElementById('category').value;
@@ -30,30 +29,4 @@ window.onload = function() {
         table.appendChild(r1);
 
     };
-
-
 }
-
-        // table.insertAdjacentHTML('afterend', '<tr><td><input type="date" name="newTransDate" placeholder="MM/DD/YYYY"/></td><td><input type="text" name="newTransDate" placeholder="MM/DD/YYYY"></td><td><div id="myDropdown" class="dropdown-content"><a href="#">Link 1</a><a href="#">Link 2</a><a href="#">Link 3</a></div></td><td><input type="number" name="Amount" placeholder="$000.00"/></td></tr>');
-
-        // <tr>
-        //     <td>
-        //         <input type="date" name="newTransDate" placeholder="MM/DD/YYYY"/>
-        //     </td>
-
-        //     <td>
-        //         <input type="text" name="newTransDate" placeholder="MM/DD/YYYY">
-        //     </td>
-
-        //     <td>
-        //         <div id="myDropdown" class="dropdown-content">
-        //             <a href="#">Link 1</a>
-        //             <a href="#">Link 2</a>
-        //             <a href="#">Link 3</a>
-        //         </div>
-        //     </td>
-
-        //     <td>
-        //         <input type="number" name="Amount" placeholder="$000.00"/>
-        //     </td>
-        // </tr>
