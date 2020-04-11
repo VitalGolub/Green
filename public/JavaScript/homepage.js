12a5219719997d5da4f4ca15d20baf7a3e8f657f
CrownPab <gagandeep.pabla@ontariotechu.net>
2020-04-11 16:22:57 -0400

    Homepage now pulls username from database and displays it in welcome screen

--- public/JavaScript/homepage.js
+++ public/JavaScript/homepage.js
@@ -2,13 +2,19 @@ window.onload = function() {
     var date = this.document.getElementsByName('date');
     var day = this.document.getElementsByName('day');
     var text = "";
+    var userName = "";
+    var introductionMessage = this.document.getElementsByName('welcomeMessage');
+   
+   
+   
+   //This ajax call get the date and day of the week and displays it on the screen
     $.ajax({
         method: 'GET',
         url: 'http://worldclockapi.com/api/json/est/now', 
         dataType: 'json',
         async: false,
         success: function (data) {
-            console.log(data);
+         
            for(var i = 0; i < 10; i++){
               text += data.currentDateTime[i];
            }
@@ -18,6 +24,22 @@ window.onload = function() {
 
         });
 
+        //this ajax call gets the username from the server and prints a welcome message
+        $.ajax({
+            method: 'GET',
+            url: 'http://localhost:3000/api/getSessionUser', 
+            dataType: 'TEXT',
+            async: false,
+            success: function (data) {
+                
+                userName += "Welcome " + data + "!";
+            
+             }
+             
+            });
+
+            introductionMessage[0].innerText = userName;
+       
 
 
 
