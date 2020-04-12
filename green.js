67158cc811439e2b4b9a46b6f81408e6a2a1ab64
Isaiah von Uders <37890836+isaiahsgithub@users.noreply.github.com>
2020-04-12 00:06:57 -0400

    Added Sockets
    For a specific user "Invalid-User" (temp), they are able to see a "Registration Log". This registration log gets new user information via sockets from the signup form. Updated the pug file.

---
+++ green.js
@@ -0,0 +1,225 @@
+var express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let session = require('express-session');
+let uuid = require('uuid/v1');
+let sqlite3 = require('sqlite3').verbose();
+//let http = require('http').createServer(app);
+
+
+//node js middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+// session tracking
+app.use(session({
+    genid: (request) => { return uuid(); },
+    resave: false, 
+    saveUninitialized: false,
+    // cookie: { secure: true},
+    secret: 'apollo slackware prepositional expectations',
+}));
+
+//sets up database for green
+let db = new sqlite3.Database('./data/green.db', (error) => {
+    if (error) {
+        console.log("Error connecting to database");
+        return;
+    }
+
+    console.log('Connected to the SQLite database');
+});
+
+//create user table inside green.db
+db.serialize(() => {
+
+    db.run(`CREATE TABLE IF NOT EXISTS users(
+        username TEXT PRIMARY KEY,
+        firstname TEXT,
+        lastname TEXT,
+        email TEXT,
+        birthDay DATE,
+        password VARCHAR(255)
+    )`);
+
+
+    db.run(`CREATE TABLE IF NOT EXISTS expenses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        date DATE,
+        amount REAL,
+        category TEXT,
+        description TEXT
+    )`);
+
+    db.run('INSERT OR IGNORE INTO users (username, firstname, lastname, email, birthday, password) VALUES (?, ?, ?, ?, ?, ?)',
+        ["test", "v", "g", "v.g@gmail.com", "a", "hello"], function(error) {
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+    });
+
+});
+
+//when no endpoint is entered the screen will default to the login page
+app.get('/' , (request,response) => {
+	response.render('login');
+});
+
+//when login is endpoint
+app.get('/login' , (request,response) => {
+    if (request.session && request.session.user && request.session.user != '') { // Check if session exists
+        response.redirect('home');
+        return;
+    }
+
+    response.render('login');
+});
+
+app.post('/processLogin', (request, response) => {
+    let username = request.body.username;
+    let password = request.body.password;
+
+    db.get(`SELECT * FROM users WHERE username = "${username}" AND password = "${password}"`, (err, row) => {
+        if (err) {
+            return;
+        }
+        if(row != null){
+            request.session.user = username;
+            console.log(`Successful login: ${username}`);
+            response.redirect('home');
+        } else {
+            // login failed
+            console.log(`Login failed: ${username}`);
+            response.redirect('login');
+        }
+    });
+});
+
+app.get('/signup' , (request,response) => {
+    //response.sendFile(__dirname + '/public/login.html')
+	response.render('signup');
+
+});
+
+app.get('/transactions' , (request,response) => {
+	response.sendFile(__dirname + '/public/transactions.html')
+});
+
+app.get('/goals' , (request,response) => {
+	response.render('goals');
+});
+
+app.get('/news' , (request,response) => {
+    response.sendFile(__dirname + '/public/news.html')
+
+});
+
+app.get('/home' , (request,response) => {
+    response.sendFile(__dirname + '/public/homepage.html')
+
+});
+
+app.get('/logout' , (request,response) => {
+    request.session.user = '';
+    response.redirect("login");
+});
+
+
+app.post('/api/getUserExpenses', (req,res) => {
+    let data = JSON.parse(JSON.stringify(req.body));
+
+    db.all(`SELECT username, date, amount, category, description FROM expenses WHERE username = "${data.username}" AND date >= "${data.from}" AND date <= "${data.to}"`, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(rows);
+    });
+});
+
+
+app.get('/api/getExpenses', (req,res) => {
+
+    db.all(`SELECT username, date, amount, category, description FROM expenses`, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(rows);
+    });
+});
+
+app.get('/api/getUsers', (req,res) => {
+
+    db.all(`SELECT * FROM users`, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(rows);
+    });
+});
+
+
+app.post('/api/addExpense', (req,res) => {
+    let data = JSON.parse(JSON.stringify(req.body));
+    console.log(data);
+
+    let user = data.username;
+    let category = data.category;
+    let amount = data.amount;
+    let description = data.description;
+    let date = data.date;
+
+    // Do something, like query a database or save data
+
+    db.run('INSERT INTO expenses (username, date, amount, category, description) VALUES (?, ?, ?, ?, ?)',
+                    [user, date, amount, category, description], function(error) {
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+    });
+});
+
+app.get('/api/getSessionUser', function(req, res) {
+    if (req.session && req.session.user) { // Check if session exists
+        let user = req.session.user;
+        res.send(user);
+    } else {
+        res.send("Invalid-User");
+    }
+  });
+
+
+app.get('/test', (request,response) => {
+    response.sendFile(__dirname + '/public/test.html');
+});
+
+
+app.set('port', 3000);
+
+var server = app.listen(app.get('port'), () => {
+    console.log('Node.js/Express is listening on port ' + app.get('port'));
+})
+
+let soio = require('socket.io')(server);
+
+soio.on('connection', function(socket) {
+	console.log('User has connected.');
+	socket.on('Leave', function(){
+		console.log('User has left.');
+	});
+	
+	socket.on('Send', function(data){
+		console.log(data.username + ": " + data.message);
+		soio.emit('admin notification', data);
+	});
+})
---
+++ public/JavaScript/homepage.js
@@ -0,0 +1,97 @@
+window.onload = function() {
+    var date = this.document.getElementsByName('date');
+    var day = this.document.getElementsByName('day');
+    var text = "";
+    var userName = "";
+    var introductionMessage = this.document.getElementsByName('welcomeMessage');
+   
+   
+	var justTheUserName = "";
+	let btnAdmin = document.getElementsByName('btnAdminLog');
+	let theModal =  document.getElementById('theModal');
+	/*theModal.style.visibility = "visible";
+	theModal.style.display = "block";
+	theModal.style.zIndex = "1";*/
+	var socket;
+	
+	var xClose = document.getElementsByClassName('close');
+	xClose[0].onclick = function(){
+		theModal.style.visibility = "hidden";
+	}
+	
+	btnAdmin[0].onclick = function(){
+		console.log('clicked');
+		theModal.style.visibility = "visible";
+		/*socket.on('admin notification', function(data) {
+			console.log('User has registered');
+		});*/
+		socket.on('admin notification', function(data) {
+			let mData = document.getElementById('mBody');
+			mData.innerHTML += "<div>" + data.username + data.message + "</div>";
+			console.log('yea');
+		})
+	
+		
+	}
+
+	
+   
+   
+   //This ajax call get the date and day of the week and displays it on the screen
+    $.ajax({
+        method: 'GET',
+        url: 'http://worldclockapi.com/api/json/est/now', 
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+         
+           for(var i = 0; i < 10; i++){
+              text += data.currentDateTime[i];
+           }
+           day[0].innerText = data.dayOfTheWeek;
+           date[0].innerText = text;
+         }
+
+        });
+
+        //this ajax call gets the username from the server and prints a welcome message
+        $.ajax({
+            method: 'GET',
+            url: 'http://localhost:3000/api/getSessionUser', 
+            dataType: 'TEXT',
+            async: false,
+            success: function (data) {
+                
+                userName += "Welcome " + data + "!";
+				justTheUserName = data;
+				
+				
+             }
+             
+            });
+
+            introductionMessage[0].innerText = userName;
+			//CHANGE TO ADMIN
+				//If the user is admin, allow them to access the registration log
+				if(justTheUserName == "Invalid-User"){
+					btnAdmin[0].style.visibility = "visible";
+					socket = io();
+				}
+				else{
+					btnAdmin[0].style.visibility = "hidden";
+				}
+}
+/*
+function checkForAdminClick(){
+	let theButton = document.getElementById()
+	let theModal =  document.getElementsByName('btnAdminLog');
+	theModal.style.visibility = "visible";
+	theModal.style.display = "block";
+	theModal.style.zIndex = "1";
+	theButton.onclick = function(){
+		console.log('clicked');
+		/*socket.on('admin notification', function(data) {
+			console.log('User has registered');
+		});*/
+	/*}
+}*/
