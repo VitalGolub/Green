afef1e03f4dd5b92ba99b1284777e4d035e67ec6
Lucas <lucassimone99@gmail.com>
2020-04-12 05:37:37 -0400

    updated goals javascript

--- public/JavaScript/goals.js
+++ public/JavaScript/goals.js
@@ -1,16 +1,36 @@
+var column_num;
+var row_num;
+var button_num;
 
 $(document).ready(function(){
     $("#buttonSet BTN").click(function() {
-
-        var column_num = parseInt( $(this).parent().parent().parent().parent().index()  ) + 1;
-        var row_num = parseInt( $(this).parent().parent().parent().parent().parent().parent().parent().index() )+1-10;
-        var button_num = parseInt( $(this).index() ) + 1;
-
-        alert( "Row_num =" + row_num + "  ,  column_num ="+ column_num +"button_num ="+ button_num);
+        column_num = parseInt( $(this).parent().parent().parent().parent().index()  ) + 1;
+        row_num = parseInt( $(this).parent().parent().parent().parent().parent().parent().parent().index() )+1-10;
+        button_num = parseInt( $(this).index() ) + 1;
     });
 });
 
+
+
 function myFunc(){
-  alert("Save the changes");
-  $(':text').val('1000');
+
+  alert(button_num);
+  switch(button_num){
+    // Set is pressed
+    case 1:
+      let goal = document.getElementById('totalGoal').value;
+
+      document.getElementById('totalGoal').value = '';
+      break;
+    //View is pressed
+    case 2:
+      break;
+    //Insert is pressed
+    case 3:
+      let amount = document.getElementById('amount').value;
+
+      document.getElementById('amount').value = '';
+      break;
+    default:
+  }
 }
