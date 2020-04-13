7c449284a1554fd8b070e8260d200405f481d6e8
Lucas <lucassimone99@gmail.com>
2020-04-13 18:27:22 -0400

    Goals now add to database

--- public/JavaScript/goals.js
+++ public/JavaScript/goals.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
         column_num = parseInt( $(this).parent().parent().parent().parent().index()  ) + 1;
         row_num = parseInt( $(this).parent().parent().parent().parent().parent().parent().parent().index() )+1-10;
         button_num = parseInt( $(this).index() ) + 1;
+
+        alert("row: "+ row_num +" col: " + column_num + " but: "+ button_num);
     });
 });
 
@@ -14,12 +16,24 @@ $(document).ready(function(){
 
 function myFunc(){
 
-  alert(button_num);
+  let user = "Invalid-User";
+  $.get("/api/getSessionUser", function(data) {
+      user = data;
+  });
+  let insertJson;
+  let category;
   switch(button_num){
     // Set is pressed
     case 1:
       let goal = document.getElementById('totalGoal').value;
-
+      category = getCategory();
+      insertJson = {
+          username:user,
+          amount:goal,
+          category:category
+      };
+      if(user != "Invalid-User")
+          $.post("/api/setGoal", insertJson);
       document.getElementById('totalGoal').value = '';
       break;
     //View is pressed
@@ -28,8 +42,66 @@ function myFunc(){
     //Insert is pressed
     case 3:
       let amount = document.getElementById('amount').value;
-
+      category = getCategory();
+      insertJson = {
+          username:user,
+          amount:amount,
+          category:category
+      };
+      if(user != "Invalid-User")
+          $.post("/api/addGoalProgress", insertJson);
       document.getElementById('amount').value = '';
+      break;
+    default:
+  }
+}
+
+function getCategory(){
+  switch(row_num){
+    case 1:
+      switch(column_num)
+      {
+        case 1:
+          return "travel";
+          break;
+        case 2:
+          return "car";
+          break
+        case 3:
+          return "education";
+          break;
+        default:
+      }
+      break;
+    case 2:
+      switch(column_num)
+      {
+        case 1:
+        return "home";
+          break;
+        case 2:
+        return "homeImprovement";
+          break
+        case 3:
+        return "retirement";
+          break;
+        default:
+      }
+      break;
+    case 3:
+      switch(column_num)
+      {
+        case 1:
+        return "creditcard";
+          break;
+        case 2:
+        return "loans";
+          break
+        case 3:
+        return "emergency";
+          break;
+        default:
+      }
       break;
     default:
   }
