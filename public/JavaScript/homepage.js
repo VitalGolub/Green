293fb24133e2846b12857c06e4d99a7efea90514
CrownPab <gagandeep.pabla@ontariotechu.net>
2020-04-13 20:29:02 -0400

    added content to about us page
    started development on about us page. Made minor changes to homepage. Added progress bar to goals

--- public/JavaScript/homepage.js
+++ public/JavaScript/homepage.js
@@ -42,7 +42,7 @@ window.onload = function() {
             async: false,
             success: function (data) {
 
-                userName += "Welcome " + data + "!";
+                userName += "Welcome, " + data + "!";
 				justTheUserName = data;
 
              }
