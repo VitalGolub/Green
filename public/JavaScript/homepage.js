cf8b005a75eb7dcff4bcea110c108b2ea5eadbd8
Atharva93 <atharva93@hotmail.com>
2020-04-14 18:18:55 -0400

    Functions Added in homepage.js
    Added the html in homepage.html for the date input and where we will draw the D3 charts. In the java script 4 functions were made displayD3(x), week(), month(), and getData(from, to). These functions are used to get the data from the database which will be sent to another function to generate the D3 charts.

--- public/JavaScript/homepage.js
+++ public/JavaScript/homepage.js
@@ -1,13 +1,15 @@
+
+var justTheUserName = "";
+var userName  = "";
+
 window.onload = function() {
     var date = this.document.getElementsByName('date');
     var day = this.document.getElementsByName('day');
     var text = "";
-    var userName = "";
     var introductionMessage = this.document.getElementsByName('welcomeMessage');
     let mData = document.getElementById('mBody');
 
 
-	var justTheUserName = "";
 	let theModal =  document.getElementById('theModal');
 	var socket;
 
@@ -42,14 +44,12 @@ window.onload = function() {
             async: false,
             success: function (data) {
 
-                userName += "Welcome, " + data + "!";
-				justTheUserName = data;
-
+				        justTheUserName = data;
+                userName = data;
+                introductionMessage[0].innerText = "Welcome, " + data + "!";
              }
-
             });
 
-            introductionMessage[0].innerText = userName;
 
 if(justTheUserName == "test"){
   //make div append to carousel array
@@ -75,39 +75,111 @@ if(justTheUserName == "test"){
   btnAdmin[0].onclick = function(){
     console.log('clicked');
     theModal.style.visibility = "visible";
-    /*socket.on('admin notification', function(data) {
-      console.log('User has registered');
-    });*/
   }
 }
-// else{
-//   btnAdmin[0].style.visibility = "hidden";
-// }
+
+  var today = new Date();
+  var dateString = today.getFullYear()+'-'+("00" + (today.getMonth()+1)).slice (-2)+'-'+("00" + today.getDate()).slice (-2);
+
+  document.getElementById('d3date').value = dateString;  
+  displayD3(0);
+  
+}
 
 
+window.onclick=function(){
+  var indicatorWrapper = document.getElementsByTagName("ol");
+  var indicator = indicatorWrapper[0].getElementsByTagName("li");
+  
+  for(let i=0; i<3; i++){
+    if($(indicator[i]).hasClass('active')){
+      displayD3(i);
+    }
+  }
+}
 
-			//CHANGE TO ADMIN
-			//If the user is admin, allow them to access the registration log
-			// if(justTheUserName == "test"){
-			// 	btnAdmin[0].style.visibility = "visible";
-			// 	socket = io();
-      //   socket.on('admin notification', function(data) {
-    	// 		mData.innerHTML += "<div>" + data.username + data.message + "</div>";
-    	// 		console.log('yea');
-    	// 	})
-      //   //make div append to carousel array
-      //   $(".carousel-inner").append(
-      //
-      //     $("<div class='carousel-item'><button type='button' class='btn' name='btnAdminLog'>View Registration Log</button></div>")
-      //   );
-      //   //make options and append
-      //   $(".carousel-indicators").append(
-      //
-      //     $("<li data-target='#carouselExampleIndicators' data-slide-to='4'></li>")
-      //   );
-      //
-			// }
-			// else{
-			// 	btnAdmin[0].style.visibility = "hidden";
-			// }
+
+function displayD3(x){
+  var returnDates;
+  switch(x){
+    case 0:
+      returnDates = week();
+      var chart = makePieChart(getData(returnDates[0],returnDates[1]));
+      break;
+    
+    case 1:
+      returnDates = month();
+      // getData(returnDates[0],returnDates[1]);
+      // console.log(getData(returnDates[0],returnDates[1]));
+      
+      break;
+
+    case 2:
+      break;
+
+    default:
+
+  }
+
+}
+
+function week(){
+  let today = new Date(document.getElementById('d3date').value);
+
+  let week = []
+  var StartOfWeek = today.getDate() - today.getDay() 
+  var day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10)
+  week.push(day)
+
+  StartOfWeek = today.getDate() - today.getDay() + 6
+  day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10)
+  week.push(day)
+
+  console.log("Week start end: " + [week[0], week[1]]);
+  return [week[0], week[1]];
+}
+
+
+function month(){
+  let today = new Date(document.getElementById('d3date').value);
+
+  var year = today.getFullYear(), month = today.getMonth();
+  var first = new Date(year, month, 1);
+  var last = new Date(year, month + 1, 0);
+  var firstDay = first.getFullYear()+'-'+("00" + (first.getMonth()+1)).slice (-2)+'-'+("00" + first.getDate()).slice (-2);
+  var lastDay = last.getFullYear()+'-'+("00" + (last.getMonth()+1)).slice (-2)+'-'+("00" + last.getDate()).slice (-2);
+
+  return [firstDay, lastDay];
+}
+
+
+function getData(from, to){
+
+  let returnJson = 
+  {
+    Entertainment:0, 
+    Education:0, 
+    Personal:0,
+    Groceries:0,
+    Restaurants:0,
+    Utilities:0,
+    Auto:0,
+    Gifts:0,
+    Investment:0
+  };
+
+  let jsonQuery = {username:userName, from:from, to:to}; 
+  $.post("/api/getUserExpenses", jsonQuery, function(data) {
+    $(jQuery.parseJSON(JSON.stringify(data))).each(function() {
+    returnJson[this.category] += this.amount;
+    });
+  });
+
+  return returnJson;
+}
+
+
+function makePieChart(addedTotal)
+{
+  
 }
