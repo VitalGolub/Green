5175220208c26a4db41335fa48592509344c5765
Atharva93 <atharva93@hotmail.com>
2020-04-15 19:03:51 -0400

    Added Pie Charts to homepage
    This commit added hardcoded pie charts to the homepage that sawitch betwwen the week and month tabs. An update button was also added for the pie chart to update when the week is changed. This commit was a paired programming effor By Lucas Simone and Atharva Shinde

--- public/JavaScript/homepage.js
+++ public/JavaScript/homepage.js
@@ -87,12 +87,15 @@ if(justTheUserName == "test"){
 }
 
 
-window.onclick=function(){
+function checkType()
+{
+  console.log("CLICKED");
   var indicatorWrapper = document.getElementsByTagName("ol");
   var indicator = indicatorWrapper[0].getElementsByTagName("li");
   
   for(let i=0; i<3; i++){
     if($(indicator[i]).hasClass('active')){
+      console.log("IIIII: ",i);
       displayD3(i);
     }
   }
@@ -104,13 +107,38 @@ function displayD3(x){
   switch(x){
     case 0:
       returnDates = week();
-      var chart = makePieChart(getData(returnDates[0],returnDates[1]));
+      // var temp2 = getData(returnDates[0],returnDates[1]);
+      // var tempData = [
+      //   {label:"Entertainment",value:3},
+      //   {label:"Education",value:4},
+      //   {label:"Personal",value:5},
+      //   {label:"Groceries",value:6},
+      //   {label:"Restaurants",value:7},
+      //   {label:"Utilities",value:8},
+      //   {label:"Auto",value:9},
+      //   {label:"Gifts",value:10},
+      //   {label:"Investment",value:13}
+      // ];
+      console.log(getData(returnDates[0],returnDates[1]));
+      change(getData(returnDates[0],returnDates[1]));
       break;
     
     case 1:
       returnDates = month();
-      // getData(returnDates[0],returnDates[1]);
-      // console.log(getData(returnDates[0],returnDates[1]));
+      var tempData = [
+        {label:"Entertainment",value:10},
+        {label:"Education",value:0},
+        {label:"Personal",value:89},
+        {label:"Groceries",value:0},
+        {label:"Restaurants",value:89.56},
+        {label:"Utilities",value:89},
+        {label:"Auto",value:89},
+        {label:"Gifts",value:89},
+        {label:"Investment",value:89}
+      ];
+      console.log(tempData);
+      // fancyPieChart(getData(returnDates[0],returnDates[1]));
+      change(tempData);
       
       break;
 
@@ -126,16 +154,15 @@ function displayD3(x){
 function week(){
   let today = new Date(document.getElementById('d3date').value);
 
-  let week = []
-  var StartOfWeek = today.getDate() - today.getDay() 
-  var day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10)
-  week.push(day)
+  let week = [];
+  var StartOfWeek = today.getDate() - today.getDay() ;
+  var day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10);
+  week.push(day);
 
-  StartOfWeek = today.getDate() - today.getDay() + 6
-  day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10)
-  week.push(day)
+  StartOfWeek = today.getDate() - today.getDay() + 6;
+  day = new Date(today.setDate(StartOfWeek)).toISOString().slice(0, 10);
+  week.push(day);
 
-  console.log("Week start end: " + [week[0], week[1]]);
   return [week[0], week[1]];
 }
 
@@ -154,32 +181,169 @@ function month(){
 
 
 function getData(from, to){
+  var returnMap = [
+    {label:"Entertainment",value:0},
+    {label:"Education",value:0},
+    {label:"Personal",value:0},
+    {label:"Groceries",value:0},
+    {label:"Restaurants",value:0},
+    {label:"Utilities",value:0},
+    {label:"Auto",value:0},
+    {label:"Gifts",value:0},
+    {label:"Investment",value:0}
+  ];
 
-  let returnJson = 
-  {
-    Entertainment:0, 
-    Education:0, 
-    Personal:0,
-    Groceries:0,
-    Restaurants:0,
-    Utilities:0,
-    Auto:0,
-    Gifts:0,
-    Investment:0
-  };
-
-  let jsonQuery = {username:userName, from:from, to:to}; 
+  var jsonQuery = {username:userName, from:from, to:to}; 
   $.post("/api/getUserExpenses", jsonQuery, function(data) {
     $(jQuery.parseJSON(JSON.stringify(data))).each(function() {
-    returnJson[this.category] += this.amount;
+      for(let i =0;i<9;i++)
+      {
+        if(returnMap[i].label == this.category)
+        {
+          returnMap[i].value += this.amount;
+        }
+      }
     });
   });
 
-  return returnJson;
+  // console.log(returnMap);
+  return returnMap; 
 }
 
 
-function makePieChart(addedTotal)
-{
-  
-}
+//STUFF FOR PIE CHART
+var svg = d3.select("#chart")
+	.append("svg")
+	.append("g")
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
+var width = 1443,
+    height = 675,
+	radius = Math.min(width, height) / 2;
+
+var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) {
+		return d.value;
+	});
+
+var arc = d3.svg.arc()
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var key = function(d){ return d.data.label; };
+
+//Modify
+var color = d3.scale.ordinal()
+	.domain(["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
+	.range(["#e54138", "#0074d9", "#7fdbff", "#39cccc", "#3d9970", "#2ecc40", "#01ff70", "#ffdc00","#ff851b"]);
+
+
+
+//Functions For pie charts
+function change(data) {
+  console.log("Data: ",data);
+  console.log("Data[0]: ",data[0]);
+  console.log("Stringify Data[0]: ",JSON.stringify(data[0]));
+  JSON.stringify(data)
+
+	/* ------- PIE SLICES -------*/
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data), key);
+
+	slice.enter()
+		.insert("path")
+		.style("fill", function(d) { return color(d.data.label); })
+		.attr("class", "slice");
+
+	slice		
+		.transition().duration(1000)
+		.attrTween("d", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				return arc(interpolate(t));
+			};
+		})
+
+	slice.exit()
+		.remove();
+
+	/* ------- TEXT LABELS -------*/
+
+	var text = svg.select(".labels").selectAll("text")
+		.data(pie(data), key);
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.text(function(d) {
+      return d.data.label;
+		});
+	
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.transition().duration(1000)
+		.attrTween("transform", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+
+	text.exit()
+		.remove();
+
+	/* ------- SLICE TO TEXT POLYLINES -------*/
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(data), key);
+	
+	polyline.enter()
+		.append("polyline");
+
+	polyline.transition().duration(1000)
+		.attrTween("points", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+				return [arc.centroid(d2), outerArc.centroid(d2), pos];
+			};			
+		});
+	
+	polyline.exit()
+		.remove();
+};
