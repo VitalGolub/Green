c172d416ce69860c73bcf8f0d33b2d842fe31400
Atharva93 <atharva93@hotmail.com>
2020-04-16 12:01:09 -0400

    Added budget table to transation tab and small css changes

--- public/JavaScript/transactions.js
+++ public/JavaScript/transactions.js
@@ -12,11 +12,11 @@ window.onload = function() {
 
         $("table").empty();
         let table=document.getElementById("table");
-        generateTable(table, user, from, to);
+        generateTable(table, user, from, to);    
     });
 
 
-    let user = "Invalid-User";
+    var user = "Invalid-User";
     $.get("/api/getSessionUser", function(data) {
         user = data;
 
@@ -25,6 +25,9 @@ window.onload = function() {
             var dateString = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
             var dateStringPrev = (today.getFullYear()-10)+'-'+(today.getMonth()+1)+'-'+today.getDate();
             generateTable(table, user, dateStringPrev, dateString);
+
+            let budgetTable =document.getElementById("editBudget");
+            generateBudgetsTable(budgetTable, user);
         }
     });
 
@@ -103,4 +106,104 @@ function generateTable(container, user, from, to){
 
         container.appendChild(table);
     });
+}
+
+
+
+function generateBudgetsTable(container, user) {
+
+    $.post("/api/getUserBudgets", {username:user}, function(data) {
+
+        let table = document.createElement('table');
+
+        let headerRow = document.createElement('tr');
+
+        let cell = document.createElement('th');
+        cell.innerHTML = "Budgets";
+        cell.colSpan = "2"; 
+        cell.style.textAlign = "center";
+        headerRow.appendChild(cell);
+
+        table.appendChild(headerRow);
+
+        $.each(jQuery.parseJSON(JSON.stringify(data[0])), function (key, data) {
+
+            if(key == "username")
+                return;
+
+            let row = document.createElement('tr');
+
+            let categoryCell = document.createElement('th');
+            categoryCell.innerHTML = convertBudgetName(key);
+
+            let amountCell = document.createElement('td');
+            amountCell.innerHTML = "$" + parseFloat(data).toFixed(2);;
+            amountCell.style.textAlign = "right"; 
+
+            $(amountCell).click(function(e) {
+
+                var text = $(this).text();
+                $(this).text('');
+                
+                //Create a new input element and add the value from the table to it
+                //Also add a key press listener that checks when enter is clicked and changes the table cell value
+                //to the new value in the input element
+                $('<input />').appendTo($(this)).val(text).select().on('keypress',function(e) {
+                    if (e.which == 13) {
+                        
+                        //Remove all of the non-numeric characters and force to 2 decimal places
+                        var enteredValue = parseFloat($(this).val().replace(/[^\d.-]/g, '')).toFixed(2);
+
+                        //Check to make sure a number was entered
+                        if(isNaN(enteredValue))
+                            enteredValue = parseFloat("0.00").toFixed(2);
+
+                        var newText = "$" + enteredValue;
+
+                        $.post("/api/setUserBudget", {username:user, category:key, amount:enteredValue});
+
+                        //remove the input element when enter is clicked
+                        $(this).parent().text(newText).find('input').remove();
+                    }
+                });
+            });
+
+            $(categoryCell).click(function() {
+                $(amountCell).click();
+            });
+
+            //Append the cell to the row and the row to the table and the table to the main div
+            row.appendChild(categoryCell);
+            row.appendChild(amountCell);
+            table.appendChild(row);
+        });
+
+        table.className += " table";
+        container.appendChild(table);
+    });
+}
+
+function convertBudgetName(name){
+    switch(name){
+        case 'entertainment':
+            return 'Entertainment';
+        case 'education':
+            return 'Education';
+        case 'health':
+            return 'Personal/Health Care';
+        case 'groceries':
+            return 'Restaurants';
+        case 'restaurants':
+            return 'Restaurants';
+        case 'utilities':
+            return 'Utilities & Bills';
+        case 'auto':
+            return 'Auto & Transportation';
+        case 'gifts':
+            return 'Gifts & Donations';
+        case 'investments':
+            return 'Investments';
+    }
+
+    return "";
 }
